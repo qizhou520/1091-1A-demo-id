d1734006c6344feed1c61813a695d73ffb233d6b
qizhou520 <72062584+qizhou520@users.noreply.github.com>
2020-11-30 21:05:44 +0800

    Add files via upload

---
+++ w07-1/nav.js
@@ -0,0 +1,33 @@
+function change(index) {
+  const p = document.getElementById('show');
+  console.log('p', p);
+  switch (index) {
+    case 1:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="../w01/intro.html"/>';
+      break;
+    case 2:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="../w01/pokemon.html"/>';
+      break;
+    case 3:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="../w02/blog.html"/>';
+      break;
+    case 4:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="../w02/imageGallery.html"/>';
+      break;
+    case 5:
+      p.innerHTML = '<iframe width=100% height=100% src="../w03/div.html"/>';
+      break;
+    case 6:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="../w03/div + tku60.html"/>';
+      break;
+    case 7:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="../w06/index.html"/>';
+      break;
+  }
+}
